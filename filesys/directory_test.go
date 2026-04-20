939262a4a90f7efb58b3e45accb8062cd46a0dfc
Tung Nguyen <2715595+lukaz17@users.noreply.github.com>
2026-04-20 15:49:07 +0700

    fix(filesys): correct slash by platform

--- filesys/directory_test.go
+++ filesys/directory_test.go
@@ -17,6 +17,7 @@
 package filesys
 
 import (
+	"path/filepath"
 	"reflect"
 	"testing"
 )
@@ -31,25 +32,25 @@ func TestListEntires(t *testing.T) {
 		results  []string
 	}{
 		{"multiple entries", []string{"../.tests/module/http", "../.tests/module/fmt"}, -1, []string{
-			"../.tests/module/http", "../.tests/module/http/1-get.md", "../.tests/module/http/2-post.md", "../.tests/module/http/3-put.md", "../.tests/module/http/4-delete.md",
-			"../.tests/module/fmt", "../.tests/module/fmt/1-printf.md", "../.tests/module/fmt/2-errorf.md",
+			filepath.FromSlash("../.tests/module/http"), filepath.FromSlash("../.tests/module/http/1-get.md"), filepath.FromSlash("../.tests/module/http/2-post.md"), filepath.FromSlash("../.tests/module/http/3-put.md"), filepath.FromSlash("../.tests/module/http/4-delete.md"),
+			filepath.FromSlash("../.tests/module/fmt"), filepath.FromSlash("../.tests/module/fmt/1-printf.md"), filepath.FromSlash("../.tests/module/fmt/2-errorf.md"),
 		}},
 		{"depth level 1", []string{"../.tests/module", "../.tests/basic"}, 1, []string{
-			"../.tests/module", "../.tests/module/fmt", "../.tests/module/http", "../.tests/module/readme.md",
-			"../.tests/basic", "../.tests/basic/1-helloworld.md",
+			filepath.FromSlash("../.tests/module"), filepath.FromSlash("../.tests/module/fmt"), filepath.FromSlash("../.tests/module/http"), filepath.FromSlash("../.tests/module/readme.md"),
+			filepath.FromSlash("../.tests/basic"), filepath.FromSlash("../.tests/basic/1-helloworld.md"),
 		}},
 		{"depth level 2", []string{"../.tests/module", "../.tests/basic"}, 2, []string{
-			"../.tests/module", "../.tests/module/fmt", "../.tests/module/fmt/1-printf.md", "../.tests/module/fmt/2-errorf.md",
-			"../.tests/module/http", "../.tests/module/http/1-get.md", "../.tests/module/http/2-post.md", "../.tests/module/http/3-put.md", "../.tests/module/http/4-delete.md",
-			"../.tests/module/readme.md",
-			"../.tests/basic", "../.tests/basic/1-helloworld.md",
+			filepath.FromSlash("../.tests/module"), filepath.FromSlash("../.tests/module/fmt"), filepath.FromSlash("../.tests/module/fmt/1-printf.md"), filepath.FromSlash("../.tests/module/fmt/2-errorf.md"),
+			filepath.FromSlash("../.tests/module/http"), filepath.FromSlash("../.tests/module/http/1-get.md"), filepath.FromSlash("../.tests/module/http/2-post.md"), filepath.FromSlash("../.tests/module/http/3-put.md"), filepath.FromSlash("../.tests/module/http/4-delete.md"),
+			filepath.FromSlash("../.tests/module/readme.md"),
+			filepath.FromSlash("../.tests/basic"), filepath.FromSlash("../.tests/basic/1-helloworld.md"),
 		}},
 		{"recursive", []string{"../.tests"}, -1, []string{
-			"../.tests", "../.tests/basic", "../.tests/basic/1-helloworld.md",
-			"../.tests/module", "../.tests/module/fmt", "../.tests/module/fmt/1-printf.md", "../.tests/module/fmt/2-errorf.md",
-			"../.tests/module/http", "../.tests/module/http/1-get.md", "../.tests/module/http/2-post.md", "../.tests/module/http/3-put.md", "../.tests/module/http/4-delete.md",
-			"../.tests/module/readme.md",
-			"../.tests/readme.md",
+			filepath.FromSlash("../.tests"), filepath.FromSlash("../.tests/basic"), filepath.FromSlash("../.tests/basic/1-helloworld.md"),
+			filepath.FromSlash("../.tests/module"), filepath.FromSlash("../.tests/module/fmt"), filepath.FromSlash("../.tests/module/fmt/1-printf.md"), filepath.FromSlash("../.tests/module/fmt/2-errorf.md"),
+			filepath.FromSlash("../.tests/module/http"), filepath.FromSlash("../.tests/module/http/1-get.md"), filepath.FromSlash("../.tests/module/http/2-post.md"), filepath.FromSlash("../.tests/module/http/3-put.md"), filepath.FromSlash("../.tests/module/http/4-delete.md"),
+			filepath.FromSlash("../.tests/module/readme.md"),
+			filepath.FromSlash("../.tests/readme.md"),
 		}},
 	}
 
--- filesys/file_test.go
+++ filesys/file_test.go
@@ -17,6 +17,7 @@
 package filesys
 
 import (
+	"path/filepath"
 	"reflect"
 	"testing"
 )
@@ -30,13 +31,13 @@ func TestList(t *testing.T) {
 		results []string
 	}{
 		{"files only", []string{"file.go"}, []string{"file.go"}},
-		{"directories only", []string{"../cmd"}, []string{"../cmd", "../cmd/args.go"}},
-		{"file and directories", []string{".", "file.go"}, []string{".", "directory.go", "directory_test.go", "file.go", "file_test.go", "logger.go", "file.go"}},
+		{"directories only", []string{"../build"}, []string{filepath.FromSlash("../build"), filepath.FromSlash("../build/ci.go"), filepath.FromSlash("../build/env.go"), filepath.FromSlash("../build/git.go"), filepath.FromSlash("../build/go.go"), filepath.FromSlash("../build/os.go")}},
+		{"file and directories", []string{".", "file.go"}, []string{".", "directory.go", "directory_test.go", "exec", filepath.FromSlash("exec/exec.go"), filepath.FromSlash("exec/ffmpeg.go"), filepath.FromSlash("exec/logger.go"), filepath.FromSlash("exec/mediainfo.go"), filepath.FromSlash("exec/mediainfo_report.go"), filepath.FromSlash("exec/mediainfo_test.go"), "file.go", "file_test.go", "logger.go", "file.go"}},
 		{"two dots", []string{"../.tests"}, []string{
-			"../.tests", "../.tests/basic", "../.tests/basic/1-helloworld.md",
-			"../.tests/module", "../.tests/module/fmt", "../.tests/module/fmt/1-printf.md", "../.tests/module/fmt/2-errorf.md",
-			"../.tests/module/http", "../.tests/module/http/1-get.md", "../.tests/module/http/2-post.md", "../.tests/module/http/3-put.md", "../.tests/module/http/4-delete.md",
-			"../.tests/module/readme.md", "../.tests/readme.md",
+			filepath.FromSlash("../.tests"), filepath.FromSlash("../.tests/basic"), filepath.FromSlash("../.tests/basic/1-helloworld.md"),
+			filepath.FromSlash("../.tests/module"), filepath.FromSlash("../.tests/module/fmt"), filepath.FromSlash("../.tests/module/fmt/1-printf.md"), filepath.FromSlash("../.tests/module/fmt/2-errorf.md"),
+			filepath.FromSlash("../.tests/module/http"), filepath.FromSlash("../.tests/module/http/1-get.md"), filepath.FromSlash("../.tests/module/http/2-post.md"), filepath.FromSlash("../.tests/module/http/3-put.md"), filepath.FromSlash("../.tests/module/http/4-delete.md"),
+			filepath.FromSlash("../.tests/module/readme.md"), filepath.FromSlash("../.tests/readme.md"),
 		}},
 	}
 	for _, tt := range tests {
